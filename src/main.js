db60fa77d619ea1c476a47317202d4ec5d73351b
HenriquePRA <henriquep.r@hotmail.com>
2021-01-04 14:35:34 -0300

    transposição do bloco de pesquisa

--- src/main.js
+++ src/main.js
@@ -31,27 +31,3 @@ navBarSearchbtn.addEventListener("click", (e) => {
         })
     })
 })
-
-// Mais avaliados
-const maisAvaliadosBtn = document.querySelector("#maisAvaliados");
-maisAvaliadosBtn.addEventListener("click", () => {
-    console.log("mais avaliados")
-})
-
-// Mais Recences
-const maisRecentesBtn = document.querySelector("#maisRecentes");
-maisRecentesBtn.addEventListener("click", () => {
-    console.log("maisRecentesBtn")
-}) 
-
-// Melhores Avaliados
-const melhoresAvaliadosBtn = document.querySelector("#melhoresAvaliados"); 
-melhoresAvaliadosBtn.addEventListener("click", () => {
-    console.log("melhoresAvaliados")
-}) 
-
-// Tendencias
-const maisVistosBtn = document.querySelector("#maisVistos");
-maisVistosBtn.addEventListener("click", () => {
-    console.log("maisVistos")
-})
